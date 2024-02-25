81230d0895a5dea4146ffae455facf51ca3c0e82
luc0x <lucascrespo.py@gmail.com>
2024-02-25 13:19:49 -0300

    Remote debug with GDB

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,8 @@
-qemu-system-i386 -cdrom ./../builds/AKICPP.iso
+type=$1
+debug="debug"
+
+if [ "$type" == "$debug" ]; then
+    qemu-system-i386 -s -S -cdrom ./../builds/AKICPP.iso
+else
+    qemu-system-i386 -cdrom ./../builds/AKICPP.iso
+fi
